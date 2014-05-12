c52c5b2e7bf79b5331b8fb81560671e891c32b3d
c3works <wlaprise@gmail.com>
2014-05-12 12:24:58 -0500

    added Cool Projects

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('c3worksGhApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
